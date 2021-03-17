b0d23af276499a4aaec985be032fb397c613556f
dps <dps@lx-01.pcalvo.local>
2021-03-17 17:23:24 -0400

    Build statement update on setup script

--- dps-setup.sh
+++ dps-setup.sh
@@ -120,7 +120,7 @@ exit
 function build {
 # Docker
     environment
-    sudo docker build -t $DockerfileName:1.0 -f $DockerfileName . --network host --add-host dockermg-01.pcalvo.local:127.0.1.1 
+    sudo docker build -t $DockerfileName:1.0 -f $DockerfileName . --network host --add-host $CONTAINER_NAME::127.0.1.1 
 exit
 }
 
